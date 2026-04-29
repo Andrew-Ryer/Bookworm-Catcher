b2902175f31232b5e2ad2487b63942bb0a2be042
moisesfelix01 <moisesxfelix@gmail.com>
2026-04-28 19:06:39 -0700

    Added Prefabs and Updated player script
    - book, dust, paper, slime particle prefabs and scripts added
    - temporary sprite sheets for books, dust, papers, slime

--- Assets/Scripts/NC/Player.cs
+++ Assets/Scripts/NC/Player.cs
@@ -40,7 +40,7 @@ public class Player : MonoBehaviour, IBookwormParent
     public event EventHandler SingleJumpActivated;
     public event EventHandler DoubleJumpActivated;
     public event EventHandler LandingActivated;
-    //public event EventHandler WalkingActivated;
+    public event EventHandler MovingActivated;
 
     private Vector2 previousPosition;
     private State currentState;
@@ -262,10 +262,6 @@ public class Player : MonoBehaviour, IBookwormParent
     {
         _dashActive = true;
         _dashTimer = 0f;
-
-        //Moises---------
-        currentState = State.Dashing;
-        //---------------
     }
 
     private void GameInput_OnJump(object sender, EventArgs e)
@@ -276,7 +272,6 @@ public class Player : MonoBehaviour, IBookwormParent
             //Debug.Log("Player_Jump");
             _jumpVelocity = 2f * apexHeight / apexTime;
             //Moises---------
-            currentState = State.SingleJump;
             SingleJumpActivated?.Invoke(this, EventArgs.Empty);
             //---------------
             _canJump = false;
@@ -287,7 +282,6 @@ public class Player : MonoBehaviour, IBookwormParent
             //Debug.Log("Player_DoubleJump");
             _jumpVelocity = 2f * apexHeight / apexTime;
             //Moises---------
-            currentState = State.DoubleJump;
             DoubleJumpActivated?.Invoke(this, EventArgs.Empty);
             //---------------
             _canJump = false;
@@ -359,7 +353,7 @@ public class Player : MonoBehaviour, IBookwormParent
         if((Vector2)transform.position != previousPosition)
         {
             //If Player is moving along a ladder
-            if(_onLadder)
+            if(_onLadder && !_isGrounded)
             {
                 currentState = State.Climbing;
             }
@@ -368,16 +362,37 @@ public class Player : MonoBehaviour, IBookwormParent
             {
                 currentState = State.Falling;
             }
+            else if(_canJump == false && _canDoubleJump == true)
+            {
+                currentState = State.SingleJump;
+            }
+            else if(_canJump == false && _canDoubleJump == false && !_isGrounded)
+            {
+                currentState = State.DoubleJump;
+            }
             //If player is moving left or right, without dashing
-            else if ((transform.position.x != previousPosition.x) && (transform.position.y == previousPosition.y) && !_dashActive)
+            else if ((transform.position.x != previousPosition.x) && (transform.position.y == previousPosition.y))
             {
                 //If going from falling to walking
                 if (previousState == State.Falling)
                 {
                     LandingActivated?.Invoke(this, EventArgs.Empty);
                 }
-                //WalkingActivated?.Invoke(this, EventArgs.Empty);
-                currentState = State.Moving;
+                if (_isGrounded)
+                {
+                    //if dashing
+                    if(_dashActive == true)
+                    {
+                        currentState = State.Dashing;
+                    }
+                    //if grounded, changing position, and not dashing
+                    else
+                    {
+                        MovingActivated?.Invoke(this, EventArgs.Empty);
+                        currentState = State.Moving;
+                    }
+                }
+                
             }
         }
         //If player is not changing direction
@@ -391,12 +406,17 @@ public class Player : MonoBehaviour, IBookwormParent
             currentState = State.Idle;
         }
 
-        //Debug.Log("Current State: " + currentState);
+        Debug.Log("Current State: " + currentState);
     }
 
     public bool isWalking()
     {
         return currentState == State.Moving;
+    }
+
+    public State GetState()
+    {
+        return currentState;
     }
     //-------------------------
     
